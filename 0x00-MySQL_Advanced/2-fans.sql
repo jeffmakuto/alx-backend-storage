cd1725061f49451abe32f4b7562054da5f88a74f
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-03-21 17:35:57 +0300

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
---- ranks country origins of brands
---- ordered by non-unique fans
-SELECT origin, SUM(fans) AS nb_fans
-	FROM metal_bands
-	GROUP BY origin
-	ORDER BY nb_fans DESC;
-
+-- script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
