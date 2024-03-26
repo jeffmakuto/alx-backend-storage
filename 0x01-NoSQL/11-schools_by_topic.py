c9ae305593603550234df8d9c1d0c1b9812d09bc
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-03-26 18:04:42 +0300

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,8 +8,10 @@ def schools_by_topic(mongo_collection, topic):
     """
      returns the list of school having a specific topic
 
-    :param mongo_collection:
-    :param topic:
-    :return:
+    args:
+        mongo_collection:
+        topic:
+    return:
+         list of school having a specific topic
     """
     return mongo_collection.find({"topics": topic})
